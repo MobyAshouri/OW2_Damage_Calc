24a10dbe65e565f2458ac1859079b5f39a906461
Moby Ashouri <droopychicken1@gmail.com>
2024-02-21 11:37:53 -0600

    Added Character portraits for ana

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
+from PIL import Image
 
 # damage = baseDamage * headshotMultiplier * (1 + totalDamageDealtAmplification) * (1 - min(0.50, totalDamageTakenReduction + armorReduction) + damageTakenAmplification)
 import customtkinter as ctk
+from tkinter import ttk
 
 import character as c
 
@@ -27,6 +29,25 @@ def main():
     charDropDownRight.grid(row=0, column=2, columnspan=2, sticky="w", padx=30)
     
     dropDownFrame.pack()
+    
+    charFrame = ctk.CTkFrame(win)
+    leftCharImage = ctk.CTkImage(light_image=Image.open("character-images/ana.png"),
+                                 dark_image=Image.open("character-images/ana.png"),
+                                 size=(200,200))
+    leftCharImageLabel = ctk.CTkLabel(charFrame, image=leftCharImage, text="")
+    leftCharImageLabel.grid(row=0, column=0)
+    
+    charImageSeparator = ttk.Separator(charFrame, orient="vertical")
+    charImageSeparator.grid(row=0, column=1, sticky="ns", padx=20)
+    
+    rightCharImage = ctk.CTkImage(light_image=Image.open("character-images/ana.png"),
+                                 dark_image=Image.open("character-images/ana.png"),
+                                 size=(200,200))
+    rightCharImageLabel = ctk.CTkLabel(charFrame, image=rightCharImage, text="")
+    rightCharImageLabel.grid(row=0, column=2, pady=20)
+    
+    charFrame.pack()
+    
     
 
     char = c.hero("Baptiste")
